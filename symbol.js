6b3412253fcc87983b9f9106452dec639be6936c
liaojin1 <1254701479@qq.com>
2019-06-20 13:53:22 +0800

    symbol

--- symbol.js
+++ symbol.js
@@ -18,3 +18,36 @@ sym // Symbol(abc)
 const sym = Symbol('foo');
 sym.description // "foo"
 
+// 作为属性名的symbol
+
+// 消除魔术字符串
+// 魔术字符串：在代码中多次出现，与代码南工程强耦合额某一个具体字符串或者数值。
+function getArea(shape, option) {
+    let area = 0;
+    switch (shape) {
+        case 'Triangle': //魔术字符串
+            area = .5 * option.width * option.height;
+            break;
+        // ...
+    }
+    return area;
+}
+getArea("Triangle", {width:10, height:20});
+// 改正后(常用的消除魔术字符串的方法，就是把它写成一个变量。)
+const shape = {
+    triangle: 'Triangle',// shape.triangle等于那个值并不重要，重要的是不会与其他的冲突。
+}
+function getnewArea (shape, option) {
+    let area = 0;
+    switch (shape) {
+        case shape.triangle: 
+            area = .5 * option.width * option.height;
+            break;
+        // ...
+    }
+    return area;
+}
+getnewArea(shape.triangle, {width:10, height:20});
+
+// 属性名的遍历，symbol作为属性名不会出现在for...in,for...of循环中，也不会被Object.keys()、Object.getOwnPropertyNames()、JSON.stringify()返回。
+// 但是他也不私有属性有一个Object.getOwnPropertySymbols方法可以获取指定对象的所有symbol属性名。
