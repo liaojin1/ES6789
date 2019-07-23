86d6eb4697217c258bae3b61023746f6a37efdfa
liaojin1 <1254701479@qq.com>
2019-07-23 10:16:19 +0800

    class

---
+++ class.js
@@ -0,0 +1,19 @@
+// ES5
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Point.prototype.toString = function() {
+    return '(' + this.x + ', ' + this.y + ')';
+}
+
+// ES6
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString() {
+        return '(' + this.x + ', ' + this.y + ')';
+    }
+}
